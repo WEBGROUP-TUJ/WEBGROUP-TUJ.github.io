20d86f73a541c9bb19b96a021b85b8a2fb1a3e0b
Timur <timakasimovkra@gmail.com>
2021-07-06 21:45:42 +0500

    new design

--- js/script.js
+++ js/script.js
@@ -1,137 +1,52 @@
+(function(){
+  'use strict';
 
-
-$('.nav-link::after').mousemove(function(){
-    $(this).css("display", "none");
-});
-
-
-var h1 = document.querySelector("#print");
-var x = h1.innerHTML;
-h1.innerHTML = "";
-var i = 0;
-function write() {
-    h1.innerHTML = h1.innerHTML + x[i];
-    i++;
-    if (i < x.length) {
-        setTimeout(write, 100);
+  class Menu {
+    constructor(settings) {
+      this.nodeMenu = settings.nodeMenu;
+      settings.nodeMenuButton.addEventListener('click', this.toggle.bind(this));
     }
 
-}
-setTimeout(function () {
-    write();
-}, 1500);
+    toggle() {
+      return this.nodeMenu.classList.toggle('js-menu_activated');
+    }
+  }
 
+  let nodeMenu = document.querySelector('body');
+  
+  new Menu({
+    nodeMenu: nodeMenu,
+    nodeMenuButton: nodeMenu.querySelector('.js-menu__toggle')
+  });
+})();
 
+$("header").append("<div class='glitch-window'></div>");
+//fill div with clone of real header
+$( "h1.glitched" ).clone().appendTo( ".glitch-window" );
 
-const menuToggle = document.querySelector('#menu-togle');
-const mobileNavContainer = document.querySelector('#mobile-nav');
+// (function(){
+//   'use strict';
 
-menuToggle.onclick = function () {
-    menuToggle.classList.toggle('menu-icon-active');
-    mobileNavContainer.classList.toggle('mobile-nav--active');
-    // if ($(".mobile-nav").css("display", "block")) {
-    //     $(".mobile-nav").css("display", "none");
-    // }
-    // else{
-    //     $(".mobile-nav").css("display", "block");
-    // }
-    
-}
+//   class Menu {
+//     constructor(settings) {
+//       this.nodeMenu = settings.nodeMenu;
+//       settings.nodeMenuButton.addEventListener('click', this.toggle.bind(this));
+//       settings.nodeMenuButton.addEventListener('click', this.css("margin-bottom", "100px"));
+//     }
 
+//     toggle() {
+//       return this.nodeMenu.classList.toggle('js-menu_activated');
+//     }
+//   }
 
-$(function () {
-    $('#nav li:has(ul)').doubleTapToGo();
-});
+//   let nodeMenu = document.querySelector('body');
+  
+//   new Menu({
+//     nodeMenu: nodeMenu,
+//     nodeMenuButton: nodeMenu.querySelector('.js-menu__toggle')
+//   });
+// })();
 
-var prev = ''
-
-$(document).ready(function () {
-    $('.slide3').owlCarousel({        
-        autoplay: true,
-        loop: true,
-        margin: 0,
-        nav: false,
-        dots: false,
-        center:false,
-        autoplayHoverPause: true,
-        smartSpeed: 1000,
-        navText:[
-           "",""
-        ],
-        responsive: {
-            0: {
-                items: 1
-            },
-        }
-    })
-    $('.slide2').owlCarousel({        
-        autoplay: true,
-        loop: true,
-        margin: 10,
-        nav: true,
-        dots: false,
-        center:false,
-        autoplayHoverPause: true,
-        smartSpeed: 1000,
-        navText:[
-           "",""
-        ],
-        responsive: {
-            0: {
-                items: 1
-            },
-            500: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            800: {
-                items: 2
-            },
-            1000: {
-                items: 3
-            },
-            1200: {
-                items: 4
-            },
-        }
-    })
-    $('.slide1').owlCarousel({
-        autoplay: true,
-        loop: true,
-        margin: 10,
-        nav: true,
-        dots: false,
-        center:false,
-        autoplayHoverPause: true,
-        smartSpeed: 1000,
-        navText:[
-           "",""
-        ],
-        responsive: {
-
-            500: {
-                items: 3
-            },
-            600: {
-                items: 3
-            },
-            700: {
-                items: 3
-            },
-            800: {
-                items: 4
-            },
-            900: {
-                items: 5
-            },
-            1000: {
-                items: 5
-            },
-            1100: {
-                items: 6
-            }
-        }
-    })  
-});
+// $(".hamburger__button").click(function () {
+//   $(this).css("margin-bottom", "100px");
+// })
