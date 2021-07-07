c0090f309a05e54a235d097970f7c1d3a8f857d7
Timur <timakasimovkra@gmail.com>
2021-07-08 01:44:22 +0500

    zamena

--- js/script.js
+++ js/script.js
@@ -24,6 +24,9 @@ $("header").append("<div class='glitch-window'></div>");
 //fill div with clone of real header
 $( "h1.glitched" ).clone().appendTo( ".glitch-window" );
 
+
+
+
 // (function(){
 //   'use strict';
 
